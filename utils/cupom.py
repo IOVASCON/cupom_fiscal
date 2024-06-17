5c65553949cffbb91d38f1ee1a9cfdcbb852eeca
Izairton Oliveira de Vasconcelos <iovascon@gmail.com>
2024-06-17 11:52:52 -0300

    "alinhamento do relatório"

--- utils/cupom.py
+++ utils/cupom.py
@@ -9,27 +9,25 @@ def gerar_cupom_fiscal(produtos, venda):
     resultado = []
     resultado.append("=" * 46)  # Linha de separação
     resultado.append("C U P O M   F I S C A L".center(46, ' '))  # Título centralizado
-    resultado.append("ITEM CÓDIGO DESCRIÇÃO".ljust(23) + "QTD.UN.VL_UNIT(R$) ST VL")  # Cabeçalho das colunas
+    resultado.append(f"{'ITEM':<5}{'CÓDIGO':<7}{'DESCRIÇÃO':<25}{'QTD.':<6}{'VL_UNIT(R$)':<10}{'ST VL':<10}")  # Cabeçalho das colunas
     resultado.append("=" * 46)  # Linha de separação
     
     # Itera sobre cada item da venda
     for indice in range(len(venda)):
-        subtotal = 0
         item += 1
-        produto = produtos.get(venda[indice][0])  # Obtém a descrição do produto pelo código
-        quantidade = venda[indice][1]  # Quantidade vendida
-        valor = venda[indice][2]  # Valor unitário
-        subtotal += quantidade * valor  # Calcula o subtotal
-        valor_total += subtotal  # Acumula o valor total
+        codigo = venda[indice][0]
+        produto = produtos.get(codigo)
+        quantidade = venda[indice][1]
+        valor_unitario = venda[indice][2]
+        subtotal = quantidade * valor_unitario
+        valor_total += subtotal
         
         # Adiciona as linhas do item ao resultado
-        resultado.append(f'{str(item).ljust(4)} {str(venda[indice][0]).ljust(4)} {produto.ljust(37)}')
-        resultado.append(f'{str(quantidade).rjust(11)}UN X {str(valor).rjust(6)} {str(subtotal).rjust(23)}')
+        resultado.append(f"{item:<5}{codigo:<7}{produto:<25}{quantidade:<6}{valor_unitario:<10.2f}{subtotal:<10.2f}")
 
     # Adiciona a linha final e o total ao resultado
     resultado.append("=" * 46)
-    resultado.append('TOTAL R$'.rjust(37))
-    resultado.append(f'{str(valor_total).rjust(37)}')
+    resultado.append(f"{'TOTAL R$':>36} {valor_total:.2f}")
 
     # Retorna o cupom fiscal como uma única string
     return "\n".join(resultado)
